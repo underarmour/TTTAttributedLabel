6417752ffc1bce479baf13a92f1c005d06633185
Matt Ruston <mruston@underarmour.com>
2020-07-15 15:00:51 -0500

    More header updates

--- Carthage/TTTAttributedLabel/TTTAttributedLabelSDK.h
+++ Carthage/TTTAttributedLabel/TTTAttributedLabelSDK.h
@@ -1,18 +1,20 @@
 //
-//  TestFramework.h
-//  TestFramework
+//  TTTAttributedLabelSDK.h
+//  TTTAttributedLabelSDK
 //
-//  Created by Matt Ruston on 7/13/20.
+//  Created by Matt Ruston on 7/15/20.
 //  Copyright © 2020 Under Armour. All rights reserved.
 //
+
+#import <Foundation/Foundation.h>
 
 //! Project version number for TTTAttributedLabelSDK.
 FOUNDATION_EXPORT double TTTAttributedLabelSDKVersionNumber;
 
-//! Project version string for TTTAttributedLabel.
+//! Project version string for TTTAttributedLabelSDK.
 FOUNDATION_EXPORT const unsigned char TTTAttributedLabelSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <TTTAttributedLabelSDK/PublicHeader.h>
 
 #import <TTTAttributedLabelSDK/TTTAttributedLabel.h>
 
