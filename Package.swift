3a4813248c9dc3e8a67260485eb9d8580921f9ac
Julian Panucci <julian.panucci@underarmour.com>
2021-02-22 14:14:00 -0600

    Fix Package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+
 let package = Package(
     name: "TTTAttributedLabel",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v12),
-        .watchOS(.v3)
-    ],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "TTTAttributedLabel",
-            targets: ["TTTAttributedLabel"])
+            targets: ["TTTAttributedLabel"]
+        )
     ],
     targets: [
         .target(
             name: "TTTAttributedLabel",
-            path: "TTTAttributedLabel")
-        
-    ],
-    swiftLanguageVersions: [.v5]
+            path: "TTTAttributedLabel",
+            exclude: ["Example", "Carthage"],
+            publicHeadersPath: "."
+        )
+    ]
 )
